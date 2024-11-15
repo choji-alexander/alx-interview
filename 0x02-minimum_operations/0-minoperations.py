c1283fe148d169f81ad279442d993f7bba279494
choji-alexander <chojialexander@gmail.com>
2024-11-15 14:32:29 +0000

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-""" lock box """
+""" Module for 0-minoperations"""
 
 
 def minOperations(n):
-    """ loock box """
-    n = len(boxes)
-    opened = set()
-    opened.add(0)
-    keys = [0]
- keys:
-        current = keys.pop()
-        for key in boxes[current]:
-            if key < n and key not in opened:
-                opened.add(key)
-                keys.append(key)
-
-    return len(opened) == n
+    """
+    minOperations
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return ops
