3e30648b47ac1113cd9a87236a3e41f0d5359faf
choji-alexander <chojialexander@gmail.com>
2024-11-15 14:21:00 +0000

    updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
+""" lock box """
+
 
 def canUnlockAll(boxes):
-    n = len(boxes)  # Total number of boxes
-    unlocked = set([0])  # Start with box 0 unlocked
-    keys = set(boxes[0])  # Collect keys from box 0
+    """ loock box """
+    n = len(boxes)
+    opened = set()
+    opened.add(0)
+    keys = [0]
 
-    # Continue processing as long as we have keys to check
     while keys:
-        key = keys.pop()  # Get one key from the set
-        
-        # If the key corresponds to a valid box and it's not already unlocked
-        if key < n and key not in unlocked:
-            unlocked.add(key)  # Unlock the box
-            keys.update(boxes[key])  # Add the keys from this newly unlocked box
+        current = keys.pop()
+        for key in boxes[current]:
+            if key < n and key not in opened:
+                opened.add(key)
+                keys.append(key)
 
-    # If all boxes are unlocked, return True
-    return len(unlocked) == n
-
-# Test cases
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # Expected output: True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # Expected output: True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # Expected output: False
+    return len(opened) == n 
